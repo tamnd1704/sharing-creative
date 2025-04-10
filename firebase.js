9e13f64e34a9c8181370aa027e5e78bfcc779531
tamnd1704 <166684886+tamnd1704@users.noreply.github.com>
2025-04-10 13:24:47 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,24 @@
-<!-- Đảm bảo file này được nhúng sau khi Firebase compat đã được import -->
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-storage.js";
 
-<script>
-  // Khởi tạo Firebase
-  const firebaseConfig = {
-    apiKey: "AIzaSyD87j3wuUiUpoO5wuE_o0rezNGsYZ3Sn4E",
-    authDomain: "sharing-file-57833.firebaseapp.com",
-    projectId: "sharing-file-57833",
-    storageBucket: "sharing-file-57833.appspot.com",
-    messagingSenderId: "68012015480",
-    appId: "1:68012015480:web:256e949001a6f6cd61928d"
-  };
+// Thay thế bằng thông tin dự án Firebase của bạn
+const firebaseConfig = {
+  apiKey: "AIzaSyD87j3wuUiUpoO5wuE_o0rezNGsYZ3Sn4E",
+  authDomain: "sharing-file-57833.firebaseapp.com",
+  projectId: "sharing-file-57833",
+  storageBucket: "sharing-file-57833.appspot.com",
+  messagingSenderId: "68012015480",
+  appId: "1:68012015480:web:256e949001a6f6cd61928d"
+};
 
-  firebase.initializeApp(firebaseConfig);
-  const db = firebase.firestore();
-  const storage = firebase.storage();
+// Khởi tạo Firebase
+const app = initializeApp(firebaseConfig);
 
-  // Nếu cần export cho global
-  window.db = db;
-  window.storage = storage;
-</script>
+// Khởi tạo Firestore & Storage
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Xuất ra cho file index.html dùng
+export { db, storage };
